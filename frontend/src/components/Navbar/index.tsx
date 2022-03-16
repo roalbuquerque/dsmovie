b858fe1fafd2567d1e7487e7028a60fbe8b46e7a
Rafael Albuquerque <rafae.albuquerque.it@gmail.com>
2022-03-16 16:48:27 -0300

    adjust link

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,12 +7,12 @@ function Navbar() {
       <nav className="container">
         <div className="dsmovie-nav-content">
           <h1>DSMovie</h1>
-          <a href="https://github.com/roalbuquerque">
+          {/* <a href="https://github.com/roalbuquerque">
             <div className="dsmovie-contact-container">
               <GithubIcon />
               <p className="dsmovie-contact-link">/roalbuquerque</p>
             </div>
-          </a>
+          </a> */}
         </div>
       </nav>
     </header>
